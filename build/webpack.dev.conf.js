1afd13e2dd3a73af4b958ea6321759ca3be5e545
stoneraoy@gmail.com <stoneraoy@gmail.com>
2019-05-12 15:37:23 +0800

    port

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -26,7 +26,7 @@ const webpackConfigDev = {
         contentBase: path.join(__dirname, "../src"),
         publicPath: '/',
         host: "10.10.15.45",
-        port: "8090",
+        port: "3000",
         overlay: true, // 浏览器页面上显示错误
         open: true, // 开启浏览器
         // stats: "errors-only", //stats: "errors-only"表示只打印错误：
